62bcb3f9ee1f671cd2e2373a2b5fb43b5d491964
Nikita Babko <niki-babko@yandex.ru>
2020-12-24 20:44:49 +0700

    Added code blank.

---
+++ server/controllers/notificationController.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const Notification = mongoose.model("Notification");
+// TODO:
+exports.readNotifications = (req, res) => {
+    //
+};
+// TODO:
+exports.getNotifications = (req, res) => {
+    //
+};
