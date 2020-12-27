2d842525f49d3fdbf6a7fd4c62289a2a0b694f27
Nikita Babko <niki-babko@yandex.ru>
2020-12-27 11:26:50 +0700

    Added state for notifications.

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,20 @@ import React, { Fragment, Suspense, lazy } from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
 import { history } from "./_helpers/history";
+import { alertActions } from "./actions/alertActions";
+import { notificationActions } from "./actions//notificationActions";
 // TODO:
 import "./styles/index.css";
 
 const NotFoundPage = lazy(() => import("./NotFoundPage/NotFoundPage"));
 const PasswordResetPage = lazy(() => import("./PasswordResetPage/PasswordResetPage"));
 const HomePage = lazy(() => import("./HomePage/HomePage"));
+const LoginPage = lazy(() => import("./LoginPage/LoginPage"));
+const RegisterPage = lazy(() => import("./RegisterPage/RegisterPage"));
+const ProfilePage = lazy(() => import("./ProfilePage/ProfilePage"));
+const UserProfile = lazy(() => import("./UserProfile/UserProfile"));
+const PostUploadPage = lazy(() => import("./PostUploadPage/PostUploadPage"));
+const PostPage = lazy(() => import("./PostPage/PostPage"));
 // TODO:
 
 class App extends React.Component {
