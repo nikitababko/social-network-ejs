15c13854fe51348db9d318f3ae839d806df765cc
Nikita Babko <niki-babko@yandex.ru>
2021-01-09 20:27:41 +0700

    Added opportunity to reset password using email.

--- server/handlers/emailHandler.js
+++ server/handlers/emailHandler.js
@@ -3,60 +3,119 @@ const nodemailer = require("nodemailer");
 const hbs = require("nodemailer-express-handlebars");
 
 exports.sendVerificationEmail = (data) => {
-    const { email, _id, username } = data;
+  const { email, _id, username } = data;
 
-    const token = jwt.sign(
-        {
-            email,
-            _id,
+  const token = jwt.sign(
+    {
+      email,
+      _id,
+    },
+    process.env.JWT_KEY,
+    {
+      expiresIn: "30m",
+    }
+  );
+
+  // config for mailserver and mail, input your data
+  const config = {
+    mailserver: {
+      service: "gmail",
+      auth: {
+        user: process.env.EMAILUSER,
+        pass: process.env.EMAILPASS,
+      },
+    },
+    mail: {
+      from: process.env.EMAILUSER,
+      to: email,
+      subject: "Account verification",
+      template: "emailVerify",
+      context: {
+        token,
+        username,
+        host: process.env.HOST,
+      },
+    },
+  };
+
+  const sendMail = async ({ mailserver, mail }) => {
+    // create a nodemailer transporter using smtp
+    let transporter = nodemailer.createTransport(mailserver);
+
+    transporter.use(
+      "compile",
+      hbs({
+        viewEngine: {
+          partialsDir: "./emailViews/",
+          defaultLayout: "",
         },
-        process.env.JWT_KEY,
-        {
-            expiresIn: "30m",
-        }
+        viewPath: "./emailViews/",
+        extName: ".hbs",
+      })
     );
 
-    // config for mailserver and mail, input your data
-    const config = {
-        mailserver: {
-            service: "gmail",
-            auth: {
-                user: process.env.EMAILUSER,
-                pass: process.env.EMAILPASS,
-            },
+    // send mail using transporter
+    await transporter.sendMail(mail);
+  };
+
+  sendMail(config).catch((err) => console.log(err));
+};
+
+exports.sendPasswordResetEmail = (data) => {
+  const { email, _id, username } = data;
+  console.log("sending email");
+  const token = jwt.sign(
+    {
+      email,
+      _id,
+    },
+    process.env.JWT_KEY,
+    {
+      expiresIn: "10m",
+    }
+  );
+
+  // config for mailserver and mail, input your data
+  const config = {
+    mailserver: {
+      service: "gmail",
+      auth: {
+        user: process.env.EMAILUSER,
+        pass: process.env.EMAILPASS,
+      },
+    },
+    mail: {
+      from: process.env.EMAILUSER,
+      to: email,
+      subject: "Password reset",
+      template: "passwordReset",
+      context: {
+        token,
+        username,
+        host: process.env.HOST,
+      },
+    },
+  };
+
+  const sendMail = async ({ mailserver, mail }) => {
+    // create a nodemailer transporter using smtp
+    let transporter = nodemailer.createTransport(mailserver);
+
+    transporter.use(
+      "compile",
+      hbs({
+        viewEngine: {
+          partialsDir: "./emailViews/",
+          defaultLayout: "",
         },
-        mail: {
-            from: process.env.EMAILUSER,
-            to: email,
-            subject: "Account verification",
-            template: "emailVerify",
-            context: {
-                token,
-                username,
-                host: process.env.HOST,
-            },
-        },
-    };
+        viewPath: "./emailViews/",
+        extName: ".hbs",
+      })
+    );
 
-    const sendMail = async ({ mailserver, mail }) => {
-        // create a nodemailer transporter using smtp
-        let transporter = nodemailer.createTransport(mailserver);
+    // send mail using transporter
+    await transporter.sendMail(mail);
+  };
 
-        transporter.use(
-            "compile",
-            hbs({
-                viewEngine: {
-                    partialsDir: "./emailViews/",
-                    defaultLayout: "",
-                },
-                viewPath: "./emailViews/",
-                extName: ".hbs",
-            })
-        );
-
-        // send mail using transporter
-        await transporter.sendMail(mail);
-    };
-
-    sendMail(config).catch((err) => console.log(err));
+  sendMail(config).catch((err) => console.log(err));
 };
