94ca50644511f8fc9949f9ea08c0c9263931bde4
Nikita Babko <niki-babko@yandex.ru>
2021-01-12 17:47:40 +0700

    Fixed bugs.

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
+const createError = require("http-errors");
 const express = require("express");
-const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const socket_io = require("socket.io");
+const jwt = require("jsonwebtoken");
 const path = require("path");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -23,18 +26,71 @@ mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 mongoose.set("autoIndex", false);
 
+require("./models/Post");
 require("./models/User");
+require("./models/Comment");
+require("./models/CommentReply");
+require("./models/CommentReplyLike");
+require("./models/CommentLike");
+require("./models/PostLike");
+require("./models/Following");
+require("./models/Followers");
+require("./models/Notification");
+require("./models/ChatRoom");
+require("./models/Message");
 
 const app = express();
+const io = socket_io();
 
 const userController = require("./controllers/userController");
+
+app.io = io;
+
+app.set("socketio", io);
+
+io.use((socket, next) => {
+    if (socket.handshake.query && socket.handshake.query.token) {
+        const token = socket.handshake.query.token.split(" ")[1];
+        jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
+            if (err) return next(new Error("Authentication error"));
+            socket.userData = decoded;
+            next();
+        });
+    } else {
+        next(new Error("Authentication error"));
+    }
+}).on("connection", (socket) => {
+    // Connection now authenticated to receive further events
+    socket.join(socket.userData.userId);
+    io.in(socket.userData.userId).clients((err, clients) => {
+        userController.changeStatus(socket.userData.userId, clients, io);
+        //console.log(clients);
+    });
+    socket.on("typing", (data) => {
+        socket.to(data.userId).emit("typing", { roomId: data.roomId });
+    });
+    socket.on("stoppedTyping", (data) => {
+        socket.to(data.userId).emit("stoppedTyping", { roomId: data.roomId });
+    });
+    socket.on("disconnect", () => {
+        socket.leave(socket.userData.userId);
+        io.in(socket.userData.userId).clients((err, clients) => {
+            userController.changeStatus(socket.userData.userId, clients, io);
+            //console.log(clients);
+        });
+    });
+});
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 200, // limit each IP to 200 requests per windowMs
 });
 
+const postsRouter = require("./routes/post");
 const usersRouter = require("./routes/user");
+const commentsRouter = require("./routes/comment");
+const notificationRouter = require("./routes/notification");
+const chatRouter = require("./routes/chat");
 
 app.use(helmet());
 if (process.env.NODE_ENV === "production") {
@@ -54,7 +110,11 @@ app.get("*", (req, res) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use("/api/post/", postsRouter);
 app.use("/api/user/", usersRouter);
+app.use("/api/comment/", commentsRouter);
+app.use("/api/notification/", notificationRouter);
+app.use("/api/chat/", chatRouter);
 
 app.get("/auth/reset/password/:jwt", function (req, res) {
     return res.status(404).json({ message: "go to port 3000" });
@@ -67,6 +127,9 @@ app.use((req, res, next) => {
 
 // error handler
 app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    // res.locals.message = err.message;
+    // res.locals.error = process.env.NODE_ENV === "development" ? err : {};
     console.log(err);
 
     // render the error page
