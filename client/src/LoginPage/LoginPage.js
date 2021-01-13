2628f61c67ba67711b35edd98b8a421b03cd3fb1
Nikita Babko <niki-babko@yandex.ru>
2021-01-13 19:41:30 +0700

    Fetch the webpack config back(reject-reverse).

--- client/src/LoginPage/LoginPage.js
+++ client/src/LoginPage/LoginPage.js
@@ -6,212 +6,208 @@ import { userActions } from "../actions/userActions";
 import Messages from "../components/Messages";
 
 class LoginPage extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    // reset login status
-    this.props.dispatch(userActions.logout());
+        // reset login status
+        this.props.dispatch(userActions.logout());
 
-    this.state = {
-      email: "",
-      password: "",
-      emailToVerify: "",
-      forgotPasswordEmail: "",
-      submitted: false,
-      showForm: false,
-      forgotPasswordForm: false,
+        this.state = {
+            email: "",
+            password: "",
+            emailToVerify: "",
+            forgotPasswordEmail: "",
+            submitted: false,
+            showForm: false,
+            forgotPasswordForm: false,
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount = () => {
+        document.title = "Login | social-network";
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  componentDidMount = () => {
-    document.title = "Login | social-network";
-  };
-
-  handleChange(e) {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  }
-
-  handleSubmit(e) {
-    e.preventDefault();
-
-    this.setState({ submitted: true });
-    const { email, password } = this.state;
-    const { dispatch } = this.props;
-    if (email && password) {
-      dispatch(userActions.login(email, password));
+    handleChange(e) {
+        const { name, value } = e.target;
+        this.setState({ [name]: value });
     }
-    //this.setState({ email: "", password: "", submitted: false });
-  }
 
-  resendEmailVerification = () => {
-    this.props.dispatch(
-      userActions.sendVerificationEmail(this.state.emailToVerify)
-    );
-  };
+    handleSubmit(e) {
+        e.preventDefault();
 
-  forgotPasswordEmail = () => {
-    this.props.dispatch(
-      userActions.sendforgotPasswordEmail(this.state.forgotPasswordEmail)
-    );
-  };
+        this.setState({ submitted: true });
+        const { email, password } = this.state;
+        const { dispatch } = this.props;
+        if (email && password) {
+            dispatch(userActions.login(email, password));
+        }
+        //this.setState({ email: "", password: "", submitted: false });
+    }
 
-  toggleEmailVerification = () => {
-    this.setState({ showForm: !this.state.showForm });
-  };
+    resendEmailVerification = () => {
+        this.props.dispatch(userActions.sendVerificationEmail(this.state.emailToVerify));
+    };
 
-  toggleForgotPasswordForm = () => {
-    this.setState({
-      forgotPasswordForm: !this.state.forgotPasswordForm,
-    });
-  };
+    forgotPasswordEmail = () => {
+        this.props.dispatch(
+            userActions.sendforgotPasswordEmail(this.state.forgotPasswordEmail)
+        );
+    };
 
-  render() {
-    const { loggingIn, alert } = this.props;
-    const {
-      email,
-      password,
-      submitted,
-      showForm,
-      forgotPasswordForm,
-    } = this.state;
-    return (
-      <div className="form-centered">
-        <Message
-          size="large"
-          attached
-          header="social-network"
-          content="Fill out the form below to log-in"
-        />
-        <Form
-          className="attached fluid segment"
-          size="large"
-          name="form"
-          success={alert.type === "success" ? true : false}
-          error={alert.type === "error" ? true : false}
-          onSubmit={this.handleSubmit}
-        >
-          <Form.Group widths="equal">
-            <Form.Input
-              autoCapitalize="none"
-              label="Email"
-              placeholder="Email or username"
-              type="text"
-              name="email"
-              value={email}
-              error={submitted && !email ? true : false}
-              onChange={this.handleChange}
-            />
+    toggleEmailVerification = () => {
+        this.setState({ showForm: !this.state.showForm });
+    };
 
-            <Form.Input
-              label="Password"
-              placeholder="Password"
-              type="password"
-              name="password"
-              value={password}
-              error={submitted && !password ? true : false}
-              onChange={this.handleChange}
-            />
-          </Form.Group>
-          <Button
-            size="large"
-            fluid
-            content="Login"
-            primary
-            disabled={email !== "" && password !== "" ? false : true}
-            loading={loggingIn ? true : false}
-          />
+    toggleForgotPasswordForm = () => {
+        this.setState({
+            forgotPasswordForm: !this.state.forgotPasswordForm,
+        });
+    };
 
-          {alert.type ? <Messages alert={alert} /> : null}
-        </Form>
-        <Message size="large" attached="bottom" warning>
-          <Icon name="help" />
-          Don't have an account?&nbsp;
-          <Link to={"/register"}>Create one now</Link>
-          <br></br>
-          <Icon name="help" />
-          Forgot password?&nbsp;
-          <span
-            onClick={this.toggleForgotPasswordForm}
-            style={{
-              textDecoration: "underline",
-              fontWeight: "900",
-              cursor: "pointer",
-            }}
-          >
-            reset password
-          </span>
-          <br></br>
-          <Icon name="envelope" />
-          Resend verification email.&nbsp;
-          <span
-            onClick={this.toggleEmailVerification}
-            style={{
-              textDecoration: "underline",
-              fontWeight: "900",
-              cursor: "pointer",
-            }}
-          >
-            resend
-          </span>
-        </Message>
-        {showForm ? (
-          <Form className="segment" size="large" name="form">
-            <Form.Field>
-              <label>Email</label>
-              <input
-                name="emailToVerify"
-                placeholder="Email"
-                type="email"
-                onChange={this.handleChange}
-              />
-            </Form.Field>
+    render() {
+        const { loggingIn, alert } = this.props;
+        const { email, password, submitted, showForm, forgotPasswordForm } = this.state;
+        return (
+            <div className="form-centered">
+                <Message
+                    size="large"
+                    attached
+                    header="social-network"
+                    content="Fill out the form below to log-in"
+                />
+                <Form
+                    className="attached fluid segment"
+                    size="large"
+                    name="form"
+                    success={alert.type === "success" ? true : false}
+                    error={alert.type === "error" ? true : false}
+                    onSubmit={this.handleSubmit}
+                >
+                    <Form.Group widths="equal">
+                        <Form.Input
+                            autoCapitalize="none"
+                            label="Email"
+                            placeholder="Email or username"
+                            type="text"
+                            name="email"
+                            value={email}
+                            error={submitted && !email ? true : false}
+                            onChange={this.handleChange}
+                        />
 
-            <Button fluid type="submit" onClick={this.resendEmailVerification}>
-              Resend
-            </Button>
-          </Form>
-        ) : null}
+                        <Form.Input
+                            label="Password"
+                            placeholder="Password"
+                            type="password"
+                            name="password"
+                            value={password}
+                            error={submitted && !password ? true : false}
+                            onChange={this.handleChange}
+                        />
+                    </Form.Group>
+                    <Button
+                        size="large"
+                        fluid
+                        content="Login"
+                        primary
+                        disabled={email !== "" && password !== "" ? false : true}
+                        loading={loggingIn ? true : false}
+                    />
 
-        {forgotPasswordForm ? (
-          <Form className="segment" size="large" name="form">
-            <Form.Field>
-              <label>Email</label>
-              <input
-                name="forgotPasswordEmail"
-                placeholder="Email"
-                type="email"
-                onChange={this.handleChange}
-              />
-            </Form.Field>
-            <Button fluid type="submit" onClick={this.forgotPasswordEmail}>
-              Send
-            </Button>
-          </Form>
-        ) : null}
+                    {alert.type ? <Messages alert={alert} /> : null}
+                </Form>
+                <Message size="large" attached="bottom" warning>
+                    <Icon name="help" />
+                    Don't have an account?&nbsp;
+                    <Link to={"/register"}>Create one now</Link>
+                    <br></br>
+                    <Icon name="help" />
+                    Forgot password?&nbsp;
+                    <span
+                        onClick={this.toggleForgotPasswordForm}
+                        style={{
+                            textDecoration: "underline",
+                            fontWeight: "900",
+                            cursor: "pointer",
+                        }}
+                    >
+                        reset password
+                    </span>
+                    <br></br>
+                    <Icon name="envelope" />
+                    Resend verification email.&nbsp;
+                    <span
+                        onClick={this.toggleEmailVerification}
+                        style={{
+                            textDecoration: "underline",
+                            fontWeight: "900",
+                            cursor: "pointer",
+                        }}
+                    >
+                        resend
+                    </span>
+                </Message>
+                {showForm ? (
+                    <Form className="segment" size="large" name="form">
+                        <Form.Field>
+                            <label>Email</label>
+                            <input
+                                name="emailToVerify"
+                                placeholder="Email"
+                                type="email"
+                                onChange={this.handleChange}
+                            />
+                        </Form.Field>
 
-        <Message>
-          <Message.Header>
-            <a
-              href="https://github.com/misa-j/social-network"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Link to a github repo
-            </a>
-          </Message.Header>
-        </Message>
-      </div>
-    );
-  }
+                        <Button
+                            fluid
+                            type="submit"
+                            onClick={this.resendEmailVerification}
+                        >
+                            Resend
+                        </Button>
+                    </Form>
+                ) : null}
+
+                {forgotPasswordForm ? (
+                    <Form className="segment" size="large" name="form">
+                        <Form.Field>
+                            <label>Email</label>
+                            <input
+                                name="forgotPasswordEmail"
+                                placeholder="Email"
+                                type="email"
+                                onChange={this.handleChange}
+                            />
+                        </Form.Field>
+                        <Button fluid type="submit" onClick={this.forgotPasswordEmail}>
+                            Send
+                        </Button>
+                    </Form>
+                ) : null}
+
+                <Message>
+                    <Message.Header>
+                        <a
+                            href="https://github.com/misa-j/social-network"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Link to a github repo
+                        </a>
+                    </Message.Header>
+                </Message>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => ({
-  loggingIn: state.authentication.loggingIn,
-  alert: state.alert,
+    loggingIn: state.authentication.loggingIn,
+    alert: state.alert,
 });
 
 const connectedLoginPage = connect(mapStateToProps)(LoginPage);
