f875580b6bb91910849400ca96a6162321e7c0ea
Nikita Babko <niki-babko@yandex.ru>
2021-01-18 19:38:44 +0700

    Replace Message to div and added new styles.

--- client/src/LoginPage/LoginPage.js
+++ client/src/LoginPage/LoginPage.js
@@ -2,8 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { Button, Form, Message, Icon } from "semantic-ui-react";
+
 import { userActions } from "../actions/userActions";
+
 import Messages from "../components/Messages";
+import Logo from "../Logo/Logo";
+
+import "./LoginPage.scss";
 
 class LoginPage extends React.Component {
     constructor(props) {
@@ -71,126 +76,131 @@ class LoginPage extends React.Component {
         const { loggingIn, alert } = this.props;
         const { email, password, submitted, showForm, forgotPasswordForm } = this.state;
         return (
-            <div className="form-centered">
-                <Message
-                    size="large"
-                    attached
-                    header="social-network"
-                    content="Fill out the form below to log-in"
-                />
-                <Form
-                    className="attached fluid segment"
-                    size="large"
-                    name="form"
-                    success={alert.type === "success" ? true : false}
-                    error={alert.type === "error" ? true : false}
-                    onSubmit={this.handleSubmit}
-                >
-                    <Form.Group widths="equal">
-                        <Form.Input
-                            autoCapitalize="none"
-                            label="Email"
-                            placeholder="Email or username"
-                            type="text"
-                            name="email"
-                            value={email}
-                            error={submitted && !email ? true : false}
-                            onChange={this.handleChange}
-                        />
-
-                        <Form.Input
-                            label="Password"
-                            placeholder="Password"
-                            type="password"
-                            name="password"
-                            value={password}
-                            error={submitted && !password ? true : false}
-                            onChange={this.handleChange}
-                        />
-                    </Form.Group>
-                    <Button
+            <div className="auth">
+                <div className="auth__column auth__left">
+                    <Logo />
+                </div>
+                <div className="auth__column auth__right form-centered">
+                    <div className="auth__header">
+                        <span className="title">NetLife</span>
+                        <span className="text">Fill out the form below to log-in</span>
+                    </div>
+                    <Form
+                        className="auth__form form attached fluid segment"
                         size="large"
-                        fluid
-                        content="Login"
-                        primary
-                        disabled={email !== "" && password !== "" ? false : true}
-                        loading={loggingIn ? true : false}
-                    />
-
-                    {alert.type ? <Messages alert={alert} /> : null}
-                </Form>
-                <Message size="large" attached="bottom" warning>
-                    <Icon name="help" />
-                    Don't have an account?&nbsp;
-                    <Link to={"/register"}>Create one now</Link>
-                    <br></br>
-                    <Icon name="help" />
-                    Forgot password?&nbsp;
-                    <span
-                        onClick={this.toggleForgotPasswordForm}
-                        style={{
-                            textDecoration: "underline",
-                            fontWeight: "900",
-                            cursor: "pointer",
-                        }}
+                        name="form"
+                        success={alert.type === "success" ? true : false}
+                        error={alert.type === "error" ? true : false}
+                        onSubmit={this.handleSubmit}
                     >
-                        reset password
-                    </span>
-                    <br></br>
-                    <Icon name="envelope" />
-                    Resend verification email.&nbsp;
-                    <span
-                        onClick={this.toggleEmailVerification}
-                        style={{
-                            textDecoration: "underline",
-                            fontWeight: "900",
-                            cursor: "pointer",
-                        }}
-                    >
-                        resend
-                    </span>
-                </Message>
-                {showForm ? (
-                    <Form className="segment" size="large" name="form">
-                        <Form.Field>
-                            <label>Email</label>
-                            <input
-                                name="emailToVerify"
-                                placeholder="Email"
-                                type="email"
+                        <Form.Group className="form__group" widths="equal">
+                            <Form.Input
+                                autoCapitalize="none"
+                                label="Email"
+                                placeholder="Email or username"
+                                type="text"
+                                name="email"
+                                value={email}
+                                error={submitted && !email ? true : false}
                                 onChange={this.handleChange}
                             />
-                        </Form.Field>
 
+                            <Form.Input
+                                label="Password"
+                                placeholder="Password"
+                                type="password"
+                                name="password"
+                                value={password}
+                                error={submitted && !password ? true : false}
+                                onChange={this.handleChange}
+                            />
+                        </Form.Group>
                         <Button
+                            size="large"
                             fluid
-                            type="submit"
-                            onClick={this.resendEmailVerification}
+                            content="Login"
+                            primary
+                            disabled={email !== "" && password !== "" ? false : true}
+                            loading={loggingIn ? true : false}
+                        />
+
+                        {alert.type ? <Messages alert={alert} /> : null}
+                    </Form>
+                    <Message size="large" attached="bottom">
+                        <Icon name="help" />
+                        Don't have an account?&nbsp;
+                        <Link to={"/register"}>Create one now</Link>
+                        <br></br>
+                        <Icon name="help" />
+                        Forgot password?&nbsp;
+                        <span
+                            onClick={this.toggleForgotPasswordForm}
+                            style={{
+                                textDecoration: "underline",
+                                fontWeight: "900",
+                                cursor: "pointer",
+                            }}
+                        >
+                            Reset password
+                        </span>
+                        <br></br>
+                        <Icon name="envelope" />
+                        Resend verification email.&nbsp;
+                        <span
+                            onClick={this.toggleEmailVerification}
+                            style={{
+                                textDecoration: "underline",
+                                fontWeight: "900",
+                                cursor: "pointer",
+                            }}
                         >
                             Resend
-                        </Button>
-                    </Form>
-                ) : null}
+                        </span>
+                    </Message>
+                    {showForm ? (
+                        <Form className="segment" size="large" name="form">
+                            <Form.Field>
+                                <label>Email</label>
+                                <input
+                                    name="emailToVerify"
+                                    placeholder="Email"
+                                    type="email"
+                                    onChange={this.handleChange}
+                                />
+                            </Form.Field>
 
-                {forgotPasswordForm ? (
-                    <Form className="segment" size="large" name="form">
-                        <Form.Field>
-                            <label>Email</label>
-                            <input
-                                name="forgotPasswordEmail"
-                                placeholder="Email"
-                                type="email"
-                                onChange={this.handleChange}
-                            />
-                        </Form.Field>
-                        <Button fluid type="submit" onClick={this.forgotPasswordEmail}>
-                            Send
-                        </Button>
-                    </Form>
-                ) : null}
+                            <Button
+                                fluid
+                                type="submit"
+                                onClick={this.resendEmailVerification}
+                            >
+                                Resend
+                            </Button>
+                        </Form>
+                    ) : null}
 
-                <Message>
-                    <Message.Header>
+                    {forgotPasswordForm ? (
+                        <Form className="segment" size="large" name="form">
+                            <Form.Field>
+                                <label>Email</label>
+                                <input
+                                    name="forgotPasswordEmail"
+                                    placeholder="Email"
+                                    type="email"
+                                    onChange={this.handleChange}
+                                />
+                            </Form.Field>
+                            <Button
+                                fluid
+                                type="submit"
+                                onClick={this.forgotPasswordEmail}
+                            >
+                                Send
+                            </Button>
+                        </Form>
+                    ) : null}
+
+                    <div className="auth__author">
                         <a
                             href="https://nikitababko.github.io/"
                             target="_blank"
@@ -198,8 +208,8 @@ class LoginPage extends React.Component {
                         >
                             Link to an author
                         </a>
-                    </Message.Header>
-                </Message>
+                    </div>
+                </div>
             </div>
         );
     }
