4d9e0ab8691971a476ba5da7c230babf357e28f3
Nikita Babko <niki-babko@yandex.ru>
2021-01-19 18:58:08 +0700

    Fixed markup.

--- client/src/MessengerPage/MessengerPage.js
+++ client/src/MessengerPage/MessengerPage.js
@@ -5,30 +5,32 @@ import { connect } from "react-redux";
 import { MessengerSidePanel } from "./MessengerSidePanel";
 import { MessengerContent } from "./MessengerContent";
 
-class MessengerPage extends Component {
-  componentDidMount = () => {
-    document.title = "Messages | social-network";
-  };
+import "./Messenger.scss";
 
-  render() {
-    const { chat, roomId } = this.props;
-    return (
-      <div id="frame">
-        <MessengerSidePanel></MessengerSidePanel>
-        {chat.currentRoom ? (
-          <MessengerContent
-            content={chat[roomId]}
-            key={roomId}
-          ></MessengerContent>
-        ) : null}
-      </div>
-    );
-  }
+class MessengerPage extends Component {
+    componentDidMount = () => {
+        document.title = "Messages | social-network";
+    };
+
+    render() {
+        const { chat, roomId } = this.props;
+        return (
+            <div id="frame">
+                <MessengerSidePanel></MessengerSidePanel>
+                {chat.currentRoom ? (
+                    <MessengerContent
+                        content={chat[roomId]}
+                        key={roomId}
+                    ></MessengerContent>
+                ) : null}
+            </div>
+        );
+    }
 }
 
-const mapStateToProps = state => ({
-  roomId: state.chat.roomId,
-  chat: state.chat
+const mapStateToProps = (state) => ({
+    roomId: state.chat.roomId,
+    chat: state.chat,
 });
 
 const connectedMessengerPage = connect(mapStateToProps)(MessengerPage);
